3ade2e7fd4a7b3acb6eb4f99ef81227ba51569e4
shataowei <taowei.sha@ymm56.com>
2020-01-14 14:54:08 +0800

    Estimator框架模版update

--- Estimator框架模版/data/LoadDemo.py
+++ Estimator框架模版/data/LoadDemo.py
@@ -8,18 +8,37 @@
 '''
 
 import tensorflow as tf
+import jieba
+import re
+
+rules = re.compile("[^\u4e00-\u9fa5a-z0-9.]")
 
 
-def input_fn(filenames="./data/knowledge.xlsx", batch_size=32, epoch_num=None, shuffle_size=256):
+def input_fn(filenames="./data/knowledge.txt", batch_size=32, epoch_num=None, shuffle_size=256):
     dataset = tf.data.TextLineDataset(filenames)
 
     def clean_data(line):
         columns_data = tf.string_split([line], '\t')
+        # tensor化
         labels = tf.string_to_number(columns_data.values[1], out_type=tf.float32)
         splits_data = columns_data.values[2]
-        return {"context": splits_data}, labels
 
-    dataset = dataset.map(clean_data)
+        def split_word(text):
+            text = text.decode()
+            print(text)
+            text = rules.sub("", text)
+            text = text.strip()
+            tokens = jieba.lcut(text)
+            print(tokens)
+            if len(tokens)==0:
+                tokens = ["未知空"]
+            # dataset需要保证长度一致
+            return tokens[:1]
+        # tf.py_func 将普通函数作用在tensor上
+        result = tf.py_func(split_word, [splits_data], [tf.string])
+        return {"context": result}, labels
+
+    dataset = dataset.map(lambda line: clean_data(line))
     # shuffle将数据打乱，数值越大，混乱程度越大
     if shuffle_size > 0:
         if epoch_num:
